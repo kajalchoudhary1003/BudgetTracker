7f446eb14f94c7cd07022f31248ae3f02642d63a
Kajal Choudhary <108188712+kajalchoudhary1003@users.noreply.github.com>
2024-06-27 05:23:44 +0530

    feat: Add authMiddleware to middleware.ts
    The `authMiddleware` is added to the `middleware.ts` file to handle authentication for certain routes. This middleware allows signed out users to access specific routes defined in the `publicRoutes` array.

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,12 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+// export default clerkMiddleware();
+
+export default authMiddleware({
+  // Allow signed out users to access the specified routes:
+  // publicRoutes: ['/anyone-can-visit-this-route'],
+});
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
