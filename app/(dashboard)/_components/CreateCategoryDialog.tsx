beaf170822f8e4ea41e973a349055d0a414ee341
Kajal Choudhary <108188712+kajalchoudhary1003@users.noreply.github.com>
2024-07-09 23:31:21 +0530

    feat: Add CategoryPicker component to CreateTransactionDialog

---
+++ app/(dashboard)/_components/CreateCategoryDialog.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { TransactionType } from "@/lib/types";
+import { cn } from "@/lib/utils";
+import {
+  CreateCategorySchema,
+  CreateCategorySchemaType,
+} from "@/schema/categories";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { PlusSquare } from "lucide-react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import CategoryPicker from "./CategoryPicker";
+import { Input } from "@/components/ui/input";
+
+interface Props {
+  type: TransactionType;
+}
+
+function CreateCategoryDialog({ type }: Props) {
+  const [open, setOpen] = useState(false);
+  const form = useForm<CreateCategorySchemaType>({
+    resolver: zodResolver(CreateCategorySchema),
+    defaultValues: {
+      type,
+    },
+  });
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button
+          variant={"ghost"}
+          className="flex border-separate items-center justify-start rounded-none border-b px-3 py-3 text-muted-foreground"
+        >
+          <PlusSquare className="mr-2 h-4 w-4" />
+          Create New
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            Create{" "}
+            <span
+              className={cn(
+                "m-1",
+                type === "income" ? "text-emerald-500" : "text-red-500"
+              )}
+            >
+              {type}
+            </span>{" "}
+            category
+          </DialogTitle>
+          <DialogDescription>
+            Categories are used to group your transactions
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form className="space-y-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input defaultValue={""} {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Transaction description (optional)
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default CreateCategoryDialog;
--- app/(dashboard)/_components/CreateTransactionDialog.tsx
+++ app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -11,10 +11,13 @@ import { TransactionType } from "@/lib/types";
 import { cn } from "@/lib/utils";
 import { CreateTransactionSchema, CreateTransactionSchemaType } from "@/schema/transaction";
 import { ReactNode } from "react";
-import { Form, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
+import { Form } from "@/components/ui/form";
+
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FormControl, FormDescription, FormField, FormItem, FormLabel } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import CategoryPicker from "./CategoryPicker";
 
 interface Props {
   trigger: ReactNode;
@@ -68,6 +71,17 @@ function CreateTransactionDialog({ trigger, type }: Props) {
                         <FormDescription>Transaction amount (required)</FormDescription>
                     </FormItem>
                 )} />
+                <div className="flex items-center justify-between gap-2">
+                <FormField control={form.control} name="category" render={({field}) => (
+                    <FormItem>
+                        <FormLabel>Category</FormLabel>
+                        <FormControl>
+                           <CategoryPicker type={type}/>
+                        </FormControl>
+                        <FormDescription>Select a category for this transaction</FormDescription>
+                    </FormItem>
+                )} />
+                </div>
             </form>
         </Form>
       </DialogContent>
