7110ab20292dcf9692cad4f3fe19cd4b1f93eb70
Kajal Choudhary <108188712+kajalchoudhary1003@users.noreply.github.com>
2024-07-09 14:59:37 +0530

    currency update feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import RootProviders from "@/components/providers/RootProviders";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,9 @@ export default function RootLayout({
     <ClerkProvider>
       {/* to change theme to light or dark */}
     <html lang="en" className="dark" style={{colorScheme: 'dark'}}>
-      <body className={inter.className}><RootProviders>{children}</RootProviders></body>
+      <body className={inter.className}>
+        <Toaster richColors position="bottom-right" />
+        <RootProviders>{children}</RootProviders></body>
     </html>
     </ClerkProvider>
   );
--- components/CurrencyComboBox.tsx
+++ components/CurrencyComboBox.tsx
@@ -19,9 +19,11 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { Currencies, Currency } from "@/lib/currencies";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import SkeletonWrapper from "./SkeletonWrapper";
 import { UserSettings } from "@prisma/client";
+import { UpdateUserCurrency } from "@/app/wizard/_actions/userSettings";
+import { toast } from "sonner";
 
 export function CurrencyComboBox() {
   const [open, setOpen] = React.useState(false);
@@ -34,7 +36,7 @@ export function CurrencyComboBox() {
     queryKey: ["userSettings"],
     queryFn: () => fetch("/api/user-settings").then((res) => res.json()),
   });
-
+ 
   React.useEffect(() => {
     if(!userSettings.data) return;
     const userCurrency = Currencies.find(
@@ -43,17 +45,46 @@ export function CurrencyComboBox() {
     if (userCurrency) setSelectedOption(userCurrency);
   }, [userSettings.data])
 
+  const mutation = useMutation({
+    mutationFn: UpdateUserCurrency,
+    onSuccess: (data: UserSettings) => {
+      toast.success('Currency updated successfully', {
+        id: 'update-currency'
+      })
+      setSelectedOption(
+        Currencies.find((c) => c.value === data.currency) || null
+      )
+    },
+    onError: (e) => {
+      toast.error("Something went wrong", {
+        id: "update-currency",
+      
+      })
+    }
+  })
+
+  const selectOption = React.useCallback((currency: Currency | null) =>{
+    if(!currency){
+      toast.error("Please select a currency")
+      return;
+    }
+    toast.loading("Updating currency...", {
+      id: "update-currency",
+    }); 
+  mutation.mutate(currency.value)
+  }, [mutation])
+
   if (isDesktop) {
     return (
       <SkeletonWrapper isLoading={userSettings.isFetching}>
       <Popover open={open} onOpenChange={setOpen}>
         <PopoverTrigger asChild>
-          <Button variant="outline" className="w-full justify-start">
+          <Button variant="outline" className="w-full justify-start" disabled={mutation.isPending}>
             {selectedOption ? <>{selectedOption.label}</> : <>Set Currency</>}
           </Button>
         </PopoverTrigger>
         <PopoverContent className="w-[200px] p-0" align="start">
-          <OptionList setOpen={setOpen} setSelectedOption={setSelectedOption} />
+          <OptionList setOpen={setOpen} setSelectedOption={selectOption} />
         </PopoverContent>
       </Popover>
       </SkeletonWrapper>
@@ -64,13 +95,13 @@ export function CurrencyComboBox() {
     <SkeletonWrapper isLoading={userSettings.isFetching}>
     <Drawer open={open} onOpenChange={setOpen}>
       <DrawerTrigger asChild>
-        <Button variant="outline" className="w-full justify-start">
+        <Button variant="outline" className="w-full justify-start" disabled={mutation.isPending}>
           {selectedOption ? <>{selectedOption.label}</> : <>Set currency</>}
         </Button>
       </DrawerTrigger>
       <DrawerContent>
         <div className="mt-4 border-t">
-          <OptionList setOpen={setOpen} setSelectedOption={setSelectedOption} />
+          <OptionList setOpen={setOpen} setSelectedOption={selectOption} />
         </div>
       </DrawerContent>
     </Drawer>
