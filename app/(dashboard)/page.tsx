480bb675b26588596a6e82388af16b2d185fead2
Kajal Choudhary <108188712+kajalchoudhary1003@users.noreply.github.com>
2024-07-30 13:15:18 +0530

    manage page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 import React from "react";
 import CreateTransactionDialog from "./_components/CreateTransactionDialog";
 import Overview from "./_components/Overview";
+import History from "./_components/History";
 
 async function page() {
   const user = await currentUser();
@@ -25,29 +26,33 @@ async function page() {
         <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
           <p className="text-3xl font-bold">Hello, {user.firstName}! 👋</p>
           <div className="flex items-center gap-3">
-            <CreateTransactionDialog trigger={
-               <Button
-               variant={"outline"}
-               className="border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700 hover:text-white"
-             >
-               New Income
-             </Button>
-            }
-            type="income"/>
-           <CreateTransactionDialog trigger={
-              <Button
-              variant={"outline"}
-              className="border-rose-500 bg-rose-950 text-white hover:bg-rose-700 hover:text-white"
-            >
-              New Expense
-            </Button>
-           }
-           type="expense"/>
-          
+            <CreateTransactionDialog
+              trigger={
+                <Button
+                  variant={"outline"}
+                  className="border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700 hover:text-white"
+                >
+                  New Income
+                </Button>
+              }
+              type="income"
+            />
+            <CreateTransactionDialog
+              trigger={
+                <Button
+                  variant={"outline"}
+                  className="border-rose-500 bg-rose-950 text-white hover:bg-rose-700 hover:text-white"
+                >
+                  New Expense
+                </Button>
+              }
+              type="expense"
+            />
           </div>
         </div>
       </div>
       <Overview userSettings={userSettings} />
+      <History userSettings={userSettings} />
     </div>
   );
 }
